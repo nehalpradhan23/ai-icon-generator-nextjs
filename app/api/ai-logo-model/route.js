a94332097362f961df1e66e2c69b843280e53556
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2025-03-26 20:42:45 +0530

    hugging face logo generation

--- app/api/ai-logo-model/route.js
+++ app/api/ai-logo-model/route.js
@@ -1,14 +1,49 @@
 import { AILogoPrompt } from "@/configs/AiModel";
+import { db } from "@/configs/Firebase.config";
+import axios from "axios";
+import { doc, setDoc } from "firebase/firestore";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  const { prompt } = await req.json();
+  const { prompt, email, title, desc } = await req.json();
 
   try {
     // generate AI text prompt for logo
     const AiPromptResult = await AILogoPrompt.sendMessage(prompt);
+    console.log(JSON.parse(AiPromptResult.response.text()).prompt);
+
     const AIPrompt = JSON.parse(AiPromptResult.response.text()).prompt;
-    return NextResponse.json(AIPrompt);
+
+    // Generate Logo from AI modal
+    const response = await axios.post(
+      "https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-dev",
+      AIPrompt,
+      {
+        headers: {
+          Authorization: "Bearer " + process.env.HUGGING_FACE_API_KEY,
+          "Content-Type": "application/json",
+        },
+        responseType: "arraybuffer",
+      }
+    );
+    // convert to base 64 image
+    const buffer = Buffer.from(response.data, "binary");
+    const base64Image = buffer.toString("base64");
+
+    const base64ImageWithMime = `data:image/png;base64,${base64Image}`;
+    console.log(base64ImageWithMime);
+
+    // save to firebase database
+    try {
+      await setDoc(
+        doc(db, "users", EmailAddress, "logos", Date.now().toString()),
+        { image: base64ImageWithMime, title: title, desc: desc }
+      );
+    } catch (error) {
+      return NextResponse.json(error);
+    }
+
+    return NextResponse.json({ image: base64ImageWithMime });
     // create AI logo Image
   } catch (error) {
     return NextResponse.json(error);
--- app/generate-logo/page.jsx
+++ app/generate-logo/page.jsx
@@ -3,10 +3,14 @@ import React, { useContext, useEffect, useState } from "react";
 import { UserDetailContext } from "../_context/UserDetailContext";
 import Prompt from "../_data/Prompt";
 import axios from "axios";
+import Image from "next/image";
 
 function GenerateLogo() {
   const { userDetail, setUserDetail } = useContext(UserDetailContext);
   const [formData, setFormData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [logoImage, setLogoImage] = useState();
+
   useEffect(() => {
     if (typeof window !== undefined && userDetail?.email) {
       const storage = localStorage.getItem("formData");
@@ -24,6 +28,7 @@ function GenerateLogo() {
   }, [formData]);
 
   const GenerateAILogo = async () => {
+    setLoading(true);
     const PROMPT = Prompt.LOGO_PROMPT.replace("{logoTitle}", formData?.title)
       .replace("{logoDesc}", formData?.desc)
       .replace("{logoColor}", formData?.palette)
@@ -33,13 +38,27 @@ function GenerateLogo() {
     console.log("prompt- ", PROMPT);
 
     // generate logo prompt from AI
-    const res = await axios.post("/api/ai-logo-model", { prompt: PROMPT });
-    console.log("ai res ############# ", res?.data);
+    const result = await axios.post("/api/ai-logo-model", {
+      prompt: PROMPT,
+      email: userDetail?.email,
+      title: formData?.title,
+      desc: formData?.desc,
+    });
+    console.log("ai result ############# ", result?.data);
+    setLogoImage(result?.data?.image);
+    setLoading(false);
 
     // generate Logo Image
   };
 
-  return <div>GenerateLogo</div>;
+  return (
+    <div>
+      <h2>{loading && "loading..."}</h2>
+      {!loading && (
+        <Image src={logoImage} alt="logo" width={200} height={200} />
+      )}
+    </div>
+  );
 }
 
 export default GenerateLogo;
